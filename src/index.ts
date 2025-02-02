4208b938965248ba231ee2997da8b41668310952
Samad Ali Shah <samadali1697@gmail.com>
2025-02-02 16:30:40 +0100

    make crud

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,13 @@ import type { Request, Response } from "express"
 import { Person } from "./types/Person"
 
 const app = express()
+app.use(express.json())
 const PORT = 4000
+const people: Person[] = [
+  { id: 1, name: "Alice", address: "Berlin, Germany", isMarried: true },
+  { id: 2, name: "Bob", address: "Munich, Germany", isMarried: false }
+]
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 })
@@ -16,11 +22,6 @@ app.get("/api/person", (req: Request, res: Response) => {
     return
   }
 
-  const people: Person[] = [
-    { id: 1, name: "Alice", address: "Berlin, Germany", isMarried: true },
-    { id: 2, name: "Bob", address: "Munich, Germany", isMarried: false }
-  ]
-
   const person = people.find(p => p.name.toLowerCase() === searchPerson.toLowerCase())
 
   if (!person) {
@@ -29,4 +30,27 @@ app.get("/api/person", (req: Request, res: Response) => {
   }
 
   res.json(person)
+})
+
+app.post("/api/person", (req: Request<Person>, res: Response) => {
+  const person: Person = req.body
+
+  people.push(person)
+
+  res.json(person)
+})
+
+app.delete("/api/person/:id", (req: Request, res: Response) => {
+  const { id } = req.params;
+  console.log(id)
+
+  const personIndex = people.findIndex(p => p.id === parseInt(id))
+  if (personIndex === -1) {
+    res.status(404).json({ error: "Person not found!" })
+    return
+  }
+  // console.log(personIndex)
+  const deletedPerson = people.splice(personIndex, 1)
+
+  res.json(deletedPerson)
 })
